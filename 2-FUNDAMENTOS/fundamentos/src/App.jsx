d57f185b8ec1db0b80f5ca04c54f5937445815cf
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-12-07 04:59:36 -0300

    Criando o primeiro componente

---
+++ 2-FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,17 @@
+//Components
+import FirstComponent from './components/FirstComponent'
+
+//Styles / CSS
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Fundamentos de React</h1>
+      <FirstComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ 2-FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,23 @@
+//Criando o primeiro componente
+
+const FirstComponent = () => {
+    //Aqui comenta assim
+
+    /*
+    Ou assim
+    */
+
+    return (
+        <>
+            <h2>
+                Meu Primeiro Componente {/*Aqui comenta assim */}
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam est, animi maiores autem architecto aperiam fugiat dolorum vitae eius non officia sit, labore ad quas! Assumenda autem rem harum expedita?
+            </p>
+        </>
+        
+    )
+}
+
+export default FirstComponent
