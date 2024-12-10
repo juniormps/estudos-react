94909bbedad6fdbcb3555f55c1cdf615f55cc684
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-12-10 14:51:46 -0300

    Update ListRender.jsx

--- 3-AVANCANDO-EM-REACT/avancando-em-react/src/components/ListRender.jsx
+++ 3-AVANCANDO-EM-REACT/avancando-em-react/src/components/ListRender.jsx
@@ -3,14 +3,27 @@ import {useState} from 'react'
 const ListRender = () => {
     const [list] = useState(["Marcio", "Junior", "Silva"])
 
+    const [users] = useState([
+        {id: 1452100, name: "Marcio", age: 37},
+        {id: 2536145, name: "Alda", age: 67},
+        {id: 3214554, name: "José", age: 6}
+    ])
+
   return (
     <>
         <div>
             <ul>
                 {/*Atentar para o return do map: aqui deve ser com parenteses (diferente do JS normal, que é com "{}") ou sem nada*/}
-                {list.map((nome) => (
-                    <li>{nome}</li>
+                {list.map((nome, i) => (
+                    <li key={i}>{nome}</li>
                 ))}
+            </ul>
+        </div>
+        <div>
+            <ul>
+                {users.map(user => 
+                    <li key={user.id}>{user.name} - {user.age}</li>
+                )}
             </ul>
         </div>
     </>
