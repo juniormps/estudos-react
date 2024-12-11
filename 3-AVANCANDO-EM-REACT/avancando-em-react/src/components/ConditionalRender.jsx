f830b3373f752d772a5239bcb469a3a6a755623c
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-12-11 12:17:16 -0300

    Update ConditionalRender.jsx

--- 3-AVANCANDO-EM-REACT/avancando-em-react/src/components/ConditionalRender.jsx
+++ 3-AVANCANDO-EM-REACT/avancando-em-react/src/components/ConditionalRender.jsx
@@ -4,12 +4,21 @@ const ConditionalRender = () => {
     const [x] = useState(true)
     const [y] = useState(false)
 
+    const [name, setName] = useState("José")
+
   return (
-    <div>
-        <h2>Isso será exibido?</h2>
-        {x && <p>Se x for verdadeiro, será exibido!</p>}
-        {!y && <p>Ou se a variável for NÃO FALSA!</p>}
-    </div>
+    <>
+        <div>
+            <h2>If Simples</h2>
+            {x && <p>Se x for verdadeiro, será exibido!</p>}
+            {!y && <p>Ou se a variável for NÃO FALSA!</p>}
+        </div>
+        <div>
+            <h2>If Ternário</h2>
+            {name === "José" ? <p>Bem vindo, {name}</p> : <p>Usuário desconhecido</p>}
+        </div>
+        <button onClick={() => setName("Samuel")}>Clique para alterar o nome</button>
+    </>
   )
 }
 
