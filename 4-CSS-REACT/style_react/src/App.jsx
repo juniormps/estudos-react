2912bc4ca905de13da8d57b62c9fa205a04b8ec2
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-12-12 16:35:10 -0300

    CSS component

--- 4-CSS-REACT/style_react/src/App.jsx
+++ 4-CSS-REACT/style_react/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
 
   return (
     <>
+      {/* CSS Global */}
       <h1>CSS com React.JS</h1>
+
+      {/* CSS de componente */}
+      <MyComponent/>
+
+      <p>Este parágrafo é do App.jsx</p>
     </>
   )
 }
