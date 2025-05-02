2509a7d0ae6b1599ac1472b0fc2ee2a75c7ce440
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-05-02 16:30:10 -0300

    resgatando dados

--- 7-REQ_HTTP/httpreact/src/App.jsx
+++ 7-REQ_HTTP/httpreact/src/App.jsx
@@ -1,11 +1,37 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products"
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  //1 - resgatando dados
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url)
+        const data = await res.json()
+        setProducts(data)
+      } catch (error) {
+        console.error(`Erro ao buscar dados: ${error}`)
+      }
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <>
       <div className="App">
         <h1>Lista de Produtos</h1>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$ {product.price}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   )
