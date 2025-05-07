993896851febf3421a5ae490a4f52ad3749aaeff
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-05-07 02:28:25 -0300

    Refatoração do App.jsx

--- 7-REQ_HTTP/httpreact/src/App.jsx
+++ 7-REQ_HTTP/httpreact/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { useState, useEffect } from 'react'
-import { useFetch } from './hooks/useFatch'  //4- Custom Hook
+import { useFetch } from './hooks/useFatch'  //Custom Hook
 
 const url = "http://localhost:3000/products"
 
@@ -9,30 +9,10 @@ function App() {
   const [name, setName] = useState("")
   const [price, setPrice] = useState("")
 
-  //4 - GET refatorado, utilizando o Custom Hook useFetch
   const { data: items, httpConfig, loading, error } = useFetch(url)
 
-  /*----------------------------------------------------------------------
-  //1 - resgatando dados (GET)
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(url)
-        const data = await res.json()
-        setProducts(data)
-      } catch (error) {
-        console.error(`Erro ao buscar dados: ${error}`)
-      }
-    }
 
-    fetchData()
-  }, [])
-  ----------------------------------------------------------------------*/
-
-
-  
-  //2 - Adicionando produtos (POST)
+  //Adicionando produtos (POST)
   
   const handlesubmit = async (e) => {
     e.preventDefault()
@@ -42,36 +22,12 @@ function App() {
       price,
     }  //Obs.: o ID é criado automaticamente, não precisa ser inserido.
 
-  /*----------------------------------------------------------------------
-    try {
-      const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(product)
-    })
-      const addedProduct = await res.json()
-      
-      setProducts((prevProducts) => [...prevProducts, addedProduct])
-
-    } catch (error) {
-       console.error(`Erro ao cadastrar o produto: ${error}`)
-    }
-    ----------------------------------------------------------------------*/
-
-    //5 - POST refatorado utilizando o custon hooh useFetch também.
     httpConfig(product, "POST")
 
     setName("")
     setPrice("")
   }
   
-
-  
-
-
-
   return (
     <>
       <div className="App">
@@ -115,3 +71,115 @@ function App() {
 }
 
 export default App
+
+
+/*
+ABAIXO, COMO SERIAM AS REQUIZIÇÕES GET E POST SEM O CUSTO HOOK USEFETCH
+------------------------------------------------------------------------
+
+import './App.css'
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products"
+
+function App() {
+  const [products, setProducts] = useState([])
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+  const { data: items } = useFetch(url)
+
+
+  //1 - resgatando dados (GET)
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url)
+        const data = await res.json()
+        setProducts(data)
+      } catch (error) {
+        console.error(`Erro ao buscar dados: ${error}`)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+
+  //2 - Adicionando produtos (POST)
+  
+  const handlesubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    }  //Obs.: o ID é criado automaticamente, não precisa ser inserido.
+
+    try {
+      const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+      const addedProduct = await res.json()
+      
+      setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    } catch (error) {
+       console.error(`Erro ao cadastrar o produto: ${error}`)
+    }
+
+    setName("")
+    setPrice("")
+  }
+  
+
+  return (
+    <>
+      <div className="App">
+        <h1>Lista de Produtos</h1>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$ {product.price}
+            </li>
+          ))}
+        </ul>
+
+        <div className="add-product">
+          <form onSubmit={handlesubmit}>
+            <label htmlFor="name">
+              Nome:
+              <input 
+                type="text"
+                name='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)} 
+              />
+            </label>
+
+            <label htmlFor="price">
+              Preço:
+              <input 
+                type="number"
+                name='price'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)} 
+              />
+            </label>
+
+            <input type="submit" value="Criar" />
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+*/
