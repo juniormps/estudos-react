a704218d6715220d6bc32946a4c8564edb180d78
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-05-19 14:11:10 -0300

    Update Product.jsx

--- 8-REACT_ROUTER/reactrouter/src/pages/Product.jsx
+++ 8-REACT_ROUTER/reactrouter/src/pages/Product.jsx
@@ -1,12 +1,30 @@
 import { useParams } from "react-router-dom"
+import { useApi } from "../hooks/useApi"
 
 const Product = () => {
     const  { id } = useParams()
+
+    //Carregamento de dado individual
+    const url = 'http://localhost:3000/products/' + id  //NÃO pode esquecer da barra "/" depois do "products"
+    const {data: product, loading, error} = useApi(url)
+
+    console.log(product)
 
   return (
 
     <>
         <p>ID do produto: {id} </p>
+
+        {error && <p>{error}</p> }
+
+        {loading && <p>Carregando...</p>}
+
+        {product && (
+            <div>
+                <h1>{product.name} </h1>
+                <p>R$ {product.price} </p>
+            </div>
+        )}
     </>
 
   )
