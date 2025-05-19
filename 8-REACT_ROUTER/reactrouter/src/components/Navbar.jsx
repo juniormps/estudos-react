b90c7f53aa86dc966f4c75859a1a476346000ffb
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-05-19 15:45:05 -0300

    Update Navbar.jsx

--- 8-REACT_ROUTER/reactrouter/src/components/Navbar.jsx
+++ 8-REACT_ROUTER/reactrouter/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import "./Navbar.css"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 
 const Navbar = () => {
@@ -7,11 +7,31 @@ const Navbar = () => {
   return (
 
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">Sobre</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">Sobre</NavLink>
     </nav>
 
   )
 }
 
 export default Navbar
+
+//O NavLink é melhor para ser utilizado em uma barra de navegação, pois oferece um recurso nativo para indicar qual link está ativo ou não.
+
+
+//por default, o NavLink já seta true na propriedade "isActive" da página que está ativa. Sendo necessário apenas criar uma class no CSS, como o nome ".active", para adicionar os estilos desejados ao link ativo.
+
+
+//Caso quera adicionar uma outra classe dinâmica ao link ativo ou não ativo, é só fazer da maneira abaixo (que é a maneira antiga).
+//<NavLink to="/" className={({isActive}) => (isActive ? "esta-ativo" : "não-ativo")} >Home</NavLink>
+
+
+/*
+ABORDAGEM COM O "LINK" OU INVÉS DO "NAVLINK"
+
+<nav>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/about">Sobre</NavLink>
+</nav>
+
+*/
