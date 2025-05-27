ae2d1e586b774d6facffc22fad09fade56367b7a
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-05-27 11:28:55 -0300

    Criação de componentes
    criação dos componentes navbar e footer e seus estilos

--- 10-PROJETO_MINIBLOG/miniblog/src/App.jsx
+++ 10-PROJETO_MINIBLOG/miniblog/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 //Pages
 import Home from './pages/home/Home'
 import About from './pages/about/About'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
  
@@ -12,12 +14,18 @@ function App() {
     <>
       <div>
         <BrowserRouter>
+
+            <Navbar/>
+
             <div className="container">
                 <Routes>
                     <Route path='/' element={<Home/>} />
                     <Route path='/about' element={<About/>} />
                 </Routes>
             </div>
+
+            <Footer/>
+            
         </BrowserRouter>
       </div>
     </>
