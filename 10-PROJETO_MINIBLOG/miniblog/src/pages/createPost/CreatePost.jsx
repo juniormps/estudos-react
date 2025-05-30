479bef4eb61fa817d90a4d58e89aa9b082aad9de
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-05-30 13:48:02 -0300

    Update CreatePost.jsx

--- 10-PROJETO_MINIBLOG/miniblog/src/pages/createPost/CreatePost.jsx
+++ 10-PROJETO_MINIBLOG/miniblog/src/pages/createPost/CreatePost.jsx
@@ -1,9 +1,9 @@
 import styles from './CreatePost.module.css'
 
-import { useState } from "react";
-//import { useInsertDocument } from "../../hooks/useInsertDocument";
-import { useNavigate } from "react-router-dom";
-import { useAuthValue } from "../../context/AuthContext";
+import { useState } from "react"
+import { useInsertDocument } from "../../hooks/useInsertDocument"
+import { useNavigate } from "react-router-dom"
+import { useAuthValue } from "../../context/AuthContext"
 
 
 const CreatePost = () => {
@@ -14,8 +14,52 @@ const CreatePost = () => {
     const [tags, setTags] = useState([])
     const [formError, setFormError] = useState("")
 
+    const { user } = useAuthValue()
+
+    const { insertDocument, response } = useInsertDocument("posts")
+
+    const navigate = useNavigate()
+
     const handleSubmit = (e) => {
         e.preventDefault()
+
+        // validate image
+        try {
+            new URL(image)
+
+        } catch (error) {
+            setFormError("A imagem precisa ser uma URL.")
+
+        }
+
+
+        // create tags array
+        const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase())
+
+
+        // check values
+        if (!title || !image || !tags || !body) {
+
+            setFormError("Por favor, preencha todos os campos!")
+
+        }
+
+
+        if (formError) return
+
+        insertDocument({
+            title,
+            image,
+            body,
+            tagsArray,
+            uid: user.uid,
+            createdBy: user.displayName
+        })
+
+
+        // redirect to home page
+        navigate("/");
+
     }
 
 
@@ -76,12 +120,11 @@ const CreatePost = () => {
                 />
             </label>
 
-            <button className='btn'>Cadastrar</button>
+            {!response.loading && <button className='btn'>Cadastrar</button>}
+            {response.loading && <button className='btn' disabled>Aguarde...</button>}
 
-            {/*{!loading && <button className='btn'>Cadastrar</button>}
-            {loading && <button className='btn' disabled>Aguarde...</button>}
-
-            {error && <p className="error">{error}</p>}*/}
+            {response.error && <p className="error">{response.error}</p>}
+            {formError && <p className="error">{formError}</p>}
             
         </form>
     </div>
