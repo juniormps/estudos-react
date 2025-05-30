9da791cabeaf26e6c7229760982a2f52077cf130
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-05-30 16:12:56 -0300

    create useFetchDocuments

--- 10-PROJETO_MINIBLOG/miniblog/src/pages/home/Home.jsx
+++ 10-PROJETO_MINIBLOG/miniblog/src/pages/home/Home.jsx
@@ -1,11 +1,55 @@
 //CSS
 import styles from "./Home.module.css"
 
+//Hooks
+import { useNavigate, Link } from 'react-router-dom'
+import { useState } from "react"
+
+//Components
+
 const Home = () => {
+
+    const [query, setQuery] = useState("")
+    const [posts] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (query) {
+            return navigate(`/search?q=${query}`)
+        }
+    }
+    
 
   return (
 
-    <div>Home</div>
+    <div className={styles.home}>
+        <h1>Veja os nossos posts mais recentes</h1>
+
+        <form className={styles.search_form} onSubmit={handleSubmit}>
+
+            <input 
+                type="text"
+                placeholder="Ou busque por tags..."
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            
+            <button className="btn btn-dark">Pesquisar</button>
+
+        </form>
+
+        <div>
+            <h1>Posts...</h1>
+
+            {posts && posts.length === 0 && (
+                <div className={styles.noposts}>
+                    <p>Não foram encontrados posts</p>
+                    <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+                </div>
+            )}
+
+        </div>
+    </div>
 
   )
 }
