99e2c48fb0db9ed7dc4fe8659c1c30deee85c1bb
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-06-02 10:58:42 -0300

    configurações da busca

--- 10-PROJETO_MINIBLOG/miniblog/src/App.jsx
+++ 10-PROJETO_MINIBLOG/miniblog/src/App.jsx
@@ -12,12 +12,14 @@ import { AuthProvider } from './context/AuthContext'
 //Pages
 import Home from './pages/home/Home'
 import About from './pages/about/About'
+import Search from './pages/search/Search'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
 import CreatePost from './pages/createPost/CreatePost'
 import Dashboard from './pages/dashboard/Dashboard'
+
 
 function App() {
 
@@ -49,6 +51,7 @@ function App() {
                     <Routes>
                         <Route path='/' element={<Home/>} />
                         <Route path='/about' element={<About/>} />
+                        <Route path='/search' element={<Search/>} />
                         <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
                         <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
                         <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login" />}/>
---
+++ 10-PROJETO_MINIBLOG/miniblog/src/pages/search/Search.jsx
@@ -0,0 +1,17 @@
+import styles from './Search.module.css'
+
+import { useuauis}
+
+const Search = () => {
+
+  return (
+
+    <div>
+        <h2>Search</h2>
+        <p>{search}</p>
+    </div>
+
+  )
+}
+
+export default Search
