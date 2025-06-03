b179fa104fe8bf429bbedad4678391565613045a
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-06-03 15:28:50 -0300

    configurando o dashboard

--- 10-PROJETO_MINIBLOG/miniblog/src/hooks/useFetchDocuments.jsx
+++ 10-PROJETO_MINIBLOG/miniblog/src/hooks/useFetchDocuments.jsx
@@ -36,6 +36,13 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
                         orderBy("createdAt", "desc")
                     )
                     
+                } else if (uid) {
+                    q = await query(
+                        collectionRef, 
+                        where("uid", "==", uid), 
+                        orderBy("createdAt", "desc")
+                    )
+
                 } else {
                     q = await query(collectionRef, orderBy("createdAt", "desc"))
                 }
