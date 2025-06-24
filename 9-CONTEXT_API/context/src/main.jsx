602b6e24e816b944fa4c61dcefd22fe74e0b8a53
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-06-24 19:51:51 -0300

    Update main.jsx

--- 9-CONTEXT_API/context/src/main.jsx
+++ 9-CONTEXT_API/context/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+//Importação dos PROVIDER
 import { CounterContextProvider } from './context/CounterContext.jsx'
 import { TitleStyleContextProvider } from './context/TitleStyleContext.jsx'
 
