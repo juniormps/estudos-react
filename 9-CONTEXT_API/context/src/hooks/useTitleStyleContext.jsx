e8b6d6d79f762752a2b9e9a53ebabb4919a9822b
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-06-24 20:48:26 -0300

    Update useTitleStyleContext.jsx

--- 9-CONTEXT_API/context/src/hooks/useTitleStyleContext.jsx
+++ 9-CONTEXT_API/context/src/hooks/useTitleStyleContext.jsx
@@ -1,3 +1,6 @@
+//Criação de um custom hook para a utilização do contexto
+//Obs.: isto não é obrigatório, mas é uma boa prática.
+
 import { useContext } from "react"
 import { TitleStyleContext } from "../context/TitleStyleContext"
 
