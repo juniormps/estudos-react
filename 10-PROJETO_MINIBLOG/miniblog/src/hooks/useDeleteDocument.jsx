b69e9f9ef90ea15a26936a3cde7ff4dd2fa409a9
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-07-16 13:19:23 -0300

    refatoração do useDeleteDocument

--- 10-PROJETO_MINIBLOG/miniblog/src/hooks/useDeleteDocument.jsx
+++ 10-PROJETO_MINIBLOG/miniblog/src/hooks/useDeleteDocument.jsx
@@ -1,62 +1,34 @@
-import { useState, useEffect, useReducer } from "react"
+import { useState, useEffect } from "react"
 import { db } from "../firebase/firestore"
 import { doc, deleteDoc } from "firebase/firestore"
 
-const initialState = {
-  loading: null,
-  error: null
-}
-
-const deleteReducer = (state, action) => {
-
-    switch (action.type) {
-        case "LOADING":
-            return { loading: true, error: null }
-
-        case "DELETED_DOC":
-            return { loading: false, error: null }
-
-        case "ERROR":
-            return { loading: false, error: action.payload }
-
-        default:
-            return state
-        
-    }
-}
-
 
 export const useDeleteDocument = (docCollection) => {
-    const [response, dispatch] = useReducer(deleteReducer, initialState)
+    
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
 
     // deal with memory leak
     const [cancelled, setCancelled] = useState(false)
 
-    const checkCancelBeforeDispatch = (action) => {
-        if (!cancelled) {
-            dispatch(action)
-        }
-    }
 
     const deleteDocument = async (id) => {
-        checkCancelBeforeDispatch({ type: "LOADING" })
+        if (cancelled) return
 
+        setLoading(true)
+    
         try {
-
             const deletedDocument = await deleteDoc(doc(db, docCollection, id))
 
-            checkCancelBeforeDispatch({
-                type: "DELETED_DOC",
-                payload: deletedDocument
-            })
-
         } catch (error) {
+            console.log(error)
+            setError(error.message)
 
-            checkCancelBeforeDispatch({ 
-                type: "ERROR", 
-                payload: error.message 
-            })
+        } finally {
+            setLoading(false)
+
         }
+
     }
 
     useEffect(() => {
@@ -64,5 +36,5 @@ export const useDeleteDocument = (docCollection) => {
     }, [])
 
 
-    return { deleteDocument, response }
+    return { deleteDocument, loading, error }
 }
